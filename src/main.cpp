d50c0269cf0ddb7269a2eef19401e1e0944d528c
Toxe <toxe@toxe.net>
2024-07-30 12:06:16 +0200

    Add minimal main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+
 #include "solver/solver.hpp"
 
 using namespace tptps;
 
 int main()
 {
+    const auto solution = solve_puzzle(Board{4, 6}, {{Tetromino::L, Tetromino::J, Tetromino::J, Tetromino::T, Tetromino::T, Tetromino::Z}});
+
+    if (!solution) {
+        std::cout << "no solution found";
+        return 1;
+    }
+
+    std::cout << solution->print();
 }
