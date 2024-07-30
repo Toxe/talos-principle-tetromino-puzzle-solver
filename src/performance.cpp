4a38999630e077710aa7cadf53804a59f420a046
Toxe <toxe@toxe.net>
2024-07-30 21:00:56 +0200

    Improve output formatting

--- src/performance.cpp
+++ src/performance.cpp
@@ -15,7 +15,7 @@ using namespace tptps;
 
 void run(const std::string_view section, const std::string_view name, const Square size, const std::vector<Tetromino>& tetrominoes)
 {
-    fmt::print("[{}] {} | ", section, name);
+    fmt::print("{:>11} | {:>16} | ", section, name);
 
     SolverStats stats;
     const auto solution = solve_puzzle(Board{size.x, size.y}, tetrominoes, stats);
@@ -28,18 +28,21 @@ void run(const std::string_view section, const std::string_view name, const Squa
 
 int main()
 {
+    fmt::print("{:>11} | {:>16} | {:>12} | {:>19} | {:>12}\n", "Section", "Name", "Checks", "Possible Placements", "Time");
+    fmt::print("------------+------------------+--------------+---------------------+-------------\n");
+
     run("World Hub A", "Tutorial Puzzle", {4, 3}, {Tetromino::J, Tetromino::J, Tetromino::Z});
-    run("World Hub A", "         Gate A", {4, 4}, {Tetromino::I, Tetromino::Z, Tetromino::J, Tetromino::L});
+    run("World Hub A", "Gate A", {4, 4}, {Tetromino::I, Tetromino::Z, Tetromino::J, Tetromino::L});
     run("World Hub A", "Laser Connector", {4, 3}, {Tetromino::L, Tetromino::T, Tetromino::T});
-    run("World Hub A", "           Cube", {4, 4}, {Tetromino::L, Tetromino::T, Tetromino::T, Tetromino::Z});
-    // run("World Hub A", "    Star Gate A", {8, 5}, {Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::J, Tetromino::Z, Tetromino::Z, Tetromino::S, Tetromino::S});
-    run("World Hub B", "         Gate B", {4, 5}, {Tetromino::I, Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::Z});
-    run("World Hub B", "            Fan", {4, 5}, {Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::S, Tetromino::Z});
-    run("World Hub B", "      Recording", {4, 5}, {Tetromino::S, Tetromino::Z, Tetromino::J, Tetromino::T, Tetromino::T});
+    run("World Hub A", "Cube", {4, 4}, {Tetromino::L, Tetromino::T, Tetromino::T, Tetromino::Z});
+    run("World Hub A", "Star Gate A", {8, 5}, {Tetromino::L, Tetromino::J, Tetromino::Z, Tetromino::Z, Tetromino::S, Tetromino::S, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::T});
+    run("World Hub B", "Gate B", {4, 5}, {Tetromino::I, Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::Z});
+    run("World Hub B", "Fan", {4, 5}, {Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::S, Tetromino::Z});
+    run("World Hub B", "Recording", {4, 5}, {Tetromino::S, Tetromino::Z, Tetromino::J, Tetromino::T, Tetromino::T});
     run("World Hub B", "Laser Connector", {7, 4}, {Tetromino::I, Tetromino::I, Tetromino::S, Tetromino::Z, Tetromino::L, Tetromino::T, Tetromino::T});
-    // run("World Hub B", "    Star Gate B", {8, 5}, {Tetromino::I, Tetromino::I, Tetromino::O, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::J, Tetromino::L, Tetromino::L});
-    run("World Hub C", "         Gate C", {4, 6}, {Tetromino::L, Tetromino::J, Tetromino::J, Tetromino::T, Tetromino::T, Tetromino::Z});
-    run("World Hub C", "       Platform", {4, 6}, {Tetromino::O, Tetromino::I, Tetromino::Z, Tetromino::L, Tetromino::T, Tetromino::T});
-    run("World Hub C", "     Blue Laser", {7, 4}, {Tetromino::I, Tetromino::Z, Tetromino::Z, Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::J});
-    run("World Hub C", " Messenger Tomb", {6, 6}, {Tetromino::O, Tetromino::O, Tetromino::L, Tetromino::L, Tetromino::J, Tetromino::J, Tetromino::I, Tetromino::Z, Tetromino::Z});
+    run("World Hub B", "Star Gate B", {8, 5}, {Tetromino::I, Tetromino::I, Tetromino::O, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::T, Tetromino::J, Tetromino::L, Tetromino::L});
+    run("World Hub C", "Gate C", {4, 6}, {Tetromino::L, Tetromino::J, Tetromino::J, Tetromino::T, Tetromino::T, Tetromino::Z});
+    run("World Hub C", "Platform", {4, 6}, {Tetromino::O, Tetromino::I, Tetromino::Z, Tetromino::L, Tetromino::T, Tetromino::T});
+    run("World Hub C", "Blue Laser", {7, 4}, {Tetromino::I, Tetromino::Z, Tetromino::Z, Tetromino::T, Tetromino::T, Tetromino::L, Tetromino::J});
+    run("World Hub C", "Messenger Tomb", {6, 6}, {Tetromino::O, Tetromino::O, Tetromino::L, Tetromino::L, Tetromino::J, Tetromino::J, Tetromino::I, Tetromino::Z, Tetromino::Z});
 }
--- src/solver/stats.cpp
+++ src/solver/stats.cpp
@@ -8,15 +8,19 @@ namespace tptps {
 
 std::string format_duration(std::chrono::nanoseconds dur)
 {
-    if (dur < 10us)
-        return fmt::format("{:.3f} us", std::chrono::duration_cast<std::chrono::duration<float, std::micro>>(dur).count());
-    else
+    if (dur < 10ns)
+        return fmt::format("{:.3f} ns", std::chrono::duration_cast<std::chrono::duration<float, std::nano>>(dur).count());
+    else if (dur < 10us)
+        return fmt::format("{:.3f} μs", std::chrono::duration_cast<std::chrono::duration<float, std::micro>>(dur).count());
+    else if (dur < 10ms)
         return fmt::format("{:.3f} ms", std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(dur).count());
+    else
+        return fmt::format("{:.3f} s ", std::chrono::duration_cast<std::chrono::duration<float>>(dur).count());
 }
 
 std::string tptps::SolverStats::print() const
 {
-    return fmt::format("checks: {:>8}, possible placements: {:>8}, {:>12}", placements_checked, possible_placements_calculated, format_duration(std::chrono::steady_clock::now() - time_begin));
+    return fmt::format("{:>12} | {:>19} | {:>12}", placements_checked, possible_placements_calculated, format_duration(std::chrono::steady_clock::now() - time_begin));
 }
 
 }  // namespace tptps
