f49ae8d6b3844d4ffad0d9a282b63f18e361ceb9
Toxe <toxe@toxe.net>
2024-07-31 09:15:32 +0200

    Improve duration formatting

--- src/solver/stats.cpp
+++ src/solver/stats.cpp
@@ -8,11 +8,11 @@ namespace tptps {
 
 std::string format_duration(std::chrono::nanoseconds dur)
 {
-    if (dur < 10ns)
+    if (dur < 1000ns)
         return fmt::format("{:.3f} ns", std::chrono::duration_cast<std::chrono::duration<float, std::nano>>(dur).count());
-    else if (dur < 10us)
+    else if (dur < 1000us)
         return fmt::format("{:.3f} μs", std::chrono::duration_cast<std::chrono::duration<float, std::micro>>(dur).count());
-    else if (dur < 10ms)
+    else if (dur < 1000ms)
         return fmt::format("{:.3f} ms", std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(dur).count());
     else
         return fmt::format("{:.3f} s ", std::chrono::duration_cast<std::chrono::duration<float>>(dur).count());
